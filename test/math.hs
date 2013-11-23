5198420a451313b91a24100c0c3a4655620b6db6
Luis Ressel <aranea@aixah.de>
2013-11-24 00:14:18 +0100

    Move PurePrgmIO to own module

--- test/math.hs
+++ test/math.hs
@@ -1,16 +1,13 @@
-{-# LANGUAGE GeneralizedNewtypeDeriving, NamedFieldPuns #-}
+{-# LANGUAGE NamedFieldPuns #-}
 
 module Main where
 
-import Control.Monad.State
-import Control.Monad.Writer
 import Test.QuickCheck
 
 import HBF.Interpreter
 import HBF.Parser
-import HBF.PrgmIO
+import HBF.PrgmIO.Pure
 import HBF.Tape.CrumbList
-import HBF.Types
 
 
 main :: IO ()
@@ -38,13 +35,3 @@ runBinaryTest BinaryTest {bf, fun} (AVal a) (AVal b) = bfRes == funRes
     where funRes = fun a b
           bfRes = head . runPurePrgmIO [a, b] . runCrumbListT . runCmds . getRight $ parseBF bf
           getRight (Right x) = x
-
-
-newtype PurePrgmIO a = PurePrgmIO {getPurePrgmIO :: WriterT [Val] (State [Val]) a} deriving (Functor, Monad, MonadState [Val], MonadWriter [Val])
-
-runPurePrgmIO :: [Val] -> PurePrgmIO a -> [Val]
-runPurePrgmIO input = flip evalState input . execWriterT . getPurePrgmIO
-
-instance PrgmIO PurePrgmIO where
-    prgmRead = state (\(x:xs) -> (x, xs))
-    prgmWrite x = tell [x]
